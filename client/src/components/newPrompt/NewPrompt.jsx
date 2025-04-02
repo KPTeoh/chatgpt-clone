48193a6da89f02c744662ded6a32d74586643f55
Khai Pong Teoh <khaipongteoh@gmail.com>
2025-04-02 20:03:57 +0800

    Implement ImageKit loading and image uploading

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,8 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
+import Upload from "../upload/Upload";
 
 const NewPrompt = () => {
   const endRef = useRef(null);
+
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
@@ -10,12 +17,19 @@ const NewPrompt = () => {
 
   return (
     <>
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
       <div className="endChat" ref={endRef}></div>
       <div className="newPrompt">
         <form className="newForm">
-          <label htmlFor="file">
-            <img src="/attachment.png" alt="" />
-          </label>
+          <Upload setImg={setImg} />
           <input id="file" type="file" multiple={false} hidden />
           <input type="text" placeholder="Ask anything...." />
           <button>
--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -1,7 +1,9 @@
-import { IKContext, IKImage, IKUpload } from "imagekitio-react";
+import { IKContext, IKUpload } from "imagekitio-react";
+import { useRef } from "react";
 
 const urlEndpoint = import.meta.env.VITE_IMAGE_KIT_ENDPOINT;
 const publicKey = import.meta.env.VITE_IMAGE_KIT_PUBLIC_KEY;
+
 const authenticator = async () => {
   try {
     const response = await fetch("http://localhost:3000/api/upload");
@@ -21,7 +23,26 @@ const authenticator = async () => {
   }
 };
 
-const Upload = () => {
+const Upload = ({ setImg }) => {
+  const ikUploadRef = useRef(null);
+  const onError = (err) => {
+    console.log("Error", err);
+  };
+
+  const onSuccess = (res) => {
+    console.log("Success", res);
+    setImg((prev) => ({ ...prev, isLoading: false, dbDate: res }));
+  };
+
+  const onUploadProgress = (progress) => {
+    console.log("Progress", progress);
+  };
+
+  const onUploadStart = (evt) => {
+    console.log("Start", evt);
+    setImg((prev) => ({ ...prev, isLoading: true }));
+  };
+
   return (
     <IKContext
       urlEndpoint={urlEndpoint}
@@ -32,7 +53,17 @@ const Upload = () => {
         fileName="test-upload.png"
         onError={onError}
         onSuccess={onSuccess}
+        useUniqueFileName={true}
+        onUploadProgress={onUploadProgress}
+        onUploadStart={onUploadStart}
+        style={{ display: "none" }}
+        ref={ikUploadRef}
       />
+      {
+        <label onClick={() => ikUploadRef.current.click()}>
+          <img src="/attachment.png" alt="" />
+        </label>
+      }
     </IKContext>
   );
 };
